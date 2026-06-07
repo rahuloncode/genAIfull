81fc818559a2f970f45aaaf232a6e6a26605016d
dev-rahulsingh <docoderahul@gmail.com>
2026-06-07 19:54:16 +0530

    Add ProtectedRoute component and update routing to include authentication

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
+import ProtectedRoute from "./features/auth/components/ProtectedRoute";
 
 const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
-  { path: "/", element: <h1>Home Screen</h1> },
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <h1>Home Screen</h1>
+      </ProtectedRoute>
+    ),
+  },
 ]);
 
 export default router;
---
+++ Frontend/src/features/auth/components/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router";
+import { useAuth } from "../hooks/useAuth.js";
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
